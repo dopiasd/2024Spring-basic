1eacd91aaf2a0e79dce762e353e3abb4789d6750
dopiasd <zip17430@icloud.com>
2024-10-26 00:05:58 +0900

    8주차

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+}
