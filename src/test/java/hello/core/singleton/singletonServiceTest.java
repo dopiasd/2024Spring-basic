1f6a068bf69ec09bb7dbcd4be413851b815cefe2
dopiasd <zip17430@icloud.com>
2024-11-08 23:24:51 +0900

    10주차

---
+++ src/test/java/hello/core/singleton/singletonServiceTest.java
@@ -0,0 +1,15 @@
+@Test
+@DisplayName("싱글톤 패턴을 적용한 객체 사용")
+
+public void singletonServiceTest() {
+    SingletonService singletonService1 = SingletonService.getInstance();
+    SingletonService singletonService2 = SingletonService.getInstance();
+
+    System.out.println("singletonService1 = " + singletonService1);
+    System.out.println("singletonService2 = " + singletonService2);
+
+    assertThat(singletonService1).isSameAs(singletonService2);
+
+    singletonService1.logic();
+}
+
